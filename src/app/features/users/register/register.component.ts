877e3d5c7c498441ea13231238eba0a3ac67f4f9
imenbouchriha12 <imenbouchriha5@gmail.com>
2025-11-10 12:11:10 +0100

    reactive form register + adress+ phones

--- src/app/features/users/register/register.component.ts
+++ src/app/features/users/register/register.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from '../../../models/user';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -17,11 +17,39 @@ export class RegisterComponent implements OnInit {
     this.formRegister = new FormGroup({
       firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
+      //todo 0=> pattern for email => a..z"@"a..z"."a..z & password => 8 char => aA1@
+      email: new FormControl('', [Validators.required,
+        Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
+      ]),
+            password: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')
+      ]),
+  // todo 1=> work on address
+        address: new FormGroup({
+        street: new FormControl('', Validators.required),
+        city: new FormControl('', Validators.required),
+        state: new FormControl('', Validators.required),
+        zip: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{4,6}$')])
+          
+      }),
+//todo 2=> work on the list of phone numbers
+            phones: new FormArray([
+        new FormControl('', [Validators.required,])
+      ])
 
     })
   }
+
+
+    get phones(): FormArray {
+    return this.formRegister.get('phones') as FormArray;
+  }
+
+  addPhone() {
+    this.phones.push(new FormControl('', [Validators.required,]));
+  }
+
   save(){
     this.user=this.formRegister.getRawValue();
     console.log(this.user);
