e2e7acd229e8c170b8e340348369511b85a8732b
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2025-11-11 16:15:51 +0100

    update from

--- src/app/features/users/register/register.component.ts
+++ src/app/features/users/register/register.component.ts
@@ -17,13 +17,24 @@ export class RegisterComponent implements OnInit {
                     [Validators.required, Validators.minLength(3)]),
       lastName: new FormControl('',
                     [Validators.required, Validators.minLength(3)]),
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
+      email: new FormControl('',
+        [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required,Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/),Validators.minLength(8)]),
+      address: new FormGroup({
+        street: new FormControl('',[Validators.required, Validators.minLength(3)]),
+        city: new FormControl('',[Validators.required, Validators.minLength(3)]),
+        state: new FormControl('',[Validators.required, Validators.minLength(3)]),
+        zip: new FormControl('',[Validators.required, Validators.minLength(4)]),
+      })
     })
     }
+  //1- pattern for the password a..zA..Z@&0..9 (8 charact)
+  //2-add the address ()
+  //3-phones[]
 
     save(){
     let user=this.gForm.getRawValue();
     //user push service => backend
+      console.log(user)
     }
 }
